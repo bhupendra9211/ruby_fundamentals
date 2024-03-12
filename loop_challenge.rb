a93aba25c2a29ff13a9731a81747539738c088b5
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-12 22:37:09 +0545

    loop challenge is done

--- loop_challenge.rb
+++ loop_challenge.rb
@@ -1,20 +1,54 @@
-puts "Enter a sentence"
+puts "Enter a sentence "
 sentence=gets.chomp
 sentence_downcase=sentence.downcase
-word=sentence_downcase.split
-i=0
-while(i<word.length) do
-    j=i+1
-    c=1
-    while(j<word.length - i) do
-        if(word[i]==word[j])
-            c +=1
+puts " "
+word=sentence_downcase.split(" ")
+# word=["Ram","Shyam","Ram","Gopal","Shyam"]
+word_count=Array.new
+count=0
+
+word.each do |i|
+    word.each do |j|
+        if (i==j) && !(word_count.include?(i))
+            count +=1
         end
-        j +=1
     end
-    puts "#{word[i]} appers #{c} times"
-    i +=1
+    if !(word_count.include?(i))
+        word_count.push(i)
+        puts "#{i} : #{count}"
+    end
+    count = 0
 end
+
+
+
+
+
+
+
+
+
+
+
+# The code below is just practice.......
+
+# puts "Enter a sentence"
+# sentence=gets.chomp
+# sentence_downcase=sentence.downcase
+# word=sentence_downcase.split
+# i=0
+# while(i<word.length) do
+#     j=i+1
+#     c=1
+#     while(j<word.length - i) do
+#         if(word[i]==word[j])
+#             c +=1
+#         end
+#         j +=1
+#     end
+#     puts "#{word[i]} appers #{c} times"
+#     i +=1
+# end
 
 
 
