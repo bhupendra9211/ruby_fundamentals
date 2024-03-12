f63bfa33024e670d69e2a35a1065f7d8e9f5b80c
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-12 14:08:01 +0545

    loop added and hash modified

---
+++ hash_task.rb
@@ -0,0 +1,25 @@
+name_list=Hash.new
+
+puts "Enter name"
+name1=gets.chomp
+name_list[name1]=name1
+
+puts "Enter name again"
+name1=gets.chomp
+name_list[name1]=name1
+
+puts name_list
+
+# puts "Enter name again"
+# name2=gets.chomp
+
+# puts "Enter name again"
+# name2=gets.chomp
+
+# puts "Enter name again"
+# name2=gets.chomp
+
+
+
+
+# puts "1st name is #{name1} and second name is #{name2}"
