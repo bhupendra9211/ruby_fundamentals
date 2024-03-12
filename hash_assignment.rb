1f0dffe1a2e9481544cab58a9b1c6e7fdce2ebbd
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-12 13:37:49 +0545

    adding hash_assignment.rb

---
+++ hash_assignment.rb
@@ -0,0 +1,29 @@
+country={
+    "Nepal"=>{
+        "population"=>"30.03 million",
+        "gdp per capital"=>1399.011,
+        "capital"=>"Kathmandu"
+    },
+    "India"=>{
+        "population"=>"1.408 billion",
+        "gdp per capital"=>2256.59,
+        "capital"=>"New Delhi"
+    },
+    "Pakistan"=>{
+        "population"=>"231.4 million",
+        "gdp per capital"=>1505.01,
+        "capital"=>"Islambad"
+    },
+    "Bangladesh"=>{
+        "population"=>"169.4 million",
+        "gdp per capital"=>2457.92,
+        "capital"=>"Dhaka"
+    },
+    "Japan"=>{
+        "population"=>"125.7 million",
+        "gdp per capital"=>39312.66,
+        "capital"=>"Tokyo"
+    }
+
+}
+puts country["Nepal"]["population"]
