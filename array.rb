8c106590ca571b65cfe1cf14c3bebfbb7ec22ee0
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-12 11:18:15 +0545

    Quotes.rb is added

--- array.rb
+++ array.rb
@@ -1,4 +1,15 @@
+# colors = [2]
+# colors=Array.new
 colors=["black","rad","purple","green"]
-# puts(colors)
-puts(colors[0])
-puts(colors.count)
+# # puts(colors)
+# puts(colors[0])
+# puts(colors.count)
+# pepole=[["Adam",21],["jake",23],["paul",30]]
+# puts(pepole[0])
+# puts(pepole[0][0])
+# puts(pepole[0][1])
+# puts(pepole)
+# puts(pepole[1][1])
+# puts(pepole[2][1])
+colors[1]="red"
+puts(colors)
---
+++ quotes.rb
@@ -0,0 +1,9 @@
+quotes=[]
+puts"There are #{quotes.length} quotes now."
+quote="I like ramen"
+puts"Adding #{quote} ....."
+quotes.push(quote)
+puts "There are #{quotes.length} quotes now. "
+quote="Momo is good too"
+puts "Adding #{quote} ......"
+# puts(quotes)
